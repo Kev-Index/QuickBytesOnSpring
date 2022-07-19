bc9f6332f2a5c8839cdc22d8b25493c077cb780d
Kev-Index <kliu0862@bths.edu>
2022-07-19 15:39:26 -0400

    Project Setup

---
+++ QuickBytes/src/main/java/com/quickbytes/QuickBytesApplication.java
@@ -0,0 +1,13 @@
+package com.quickbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickBytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickBytesApplication.class, args);
+	}
+
+}
