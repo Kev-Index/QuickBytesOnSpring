d3f21693b9f8b8121675bef806d7115ed15651a6
Kev-Index <kliu0862@bths.edu>
2022-07-20 10:11:50 -0400

    Added OrderItem post & put

--- QuickBytes/src/main/java/com/quickbytes/controller/OrderItemController.java
+++ QuickBytes/src/main/java/com/quickbytes/controller/OrderItemController.java
@@ -3,9 +3,13 @@ package com.quickbytes.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quickbytes.model.Order;
@@ -25,12 +29,6 @@ public class OrderItemController {
 	@Autowired
 	ItemRepository itemRepository;
 
-	/* GET ALL ORDER ITEMS */
-	@GetMapping("/orderitems")
-	public List<OrderItem> getAllOrderItems() {
-		return orderItemRepository.findAll();
-	}
-	
 	/* GET ORDER ITEMS BY ID */
 	@GetMapping("/orderitem/{id}")
 	public OrderItem getOrderItem(@PathVariable("id") Long id) {
@@ -53,9 +51,60 @@ public class OrderItemController {
 		return orderItemRepository.getOrderItemsByItemId(iid);
 	}
 	
+	/* GET ALL ORDER ITEMS */
+	@GetMapping("/orderitems")
+	public List<OrderItem> getAllOrderItems() {
+		return orderItemRepository.findAll();
+	}
+	
 	/* POST ORDER ITEM */
+	@PostMapping("/orderitem/{oid}/{iid}")
+	public OrderItem postOrderItem(@RequestBody OrderItem orderItem, @PathVariable("oid") Long oid, @PathVariable("iid") Long iid) {
+		//get order
+		Optional<Order> optionalOrder = orderRepository.findById(oid);
+		if (!optionalOrder.isPresent()) {
+			throw new RuntimeException("Order ID is invalid");
+		}
+		Order order = optionalOrder.get();
+		
+		//get item
+		Optional<Item> optionalItem = itemRepository.findById(iid);
+		if (!optionalItem.isPresent()) {
+			throw new RuntimeException("Item ID is invalid");
+		}
+		Item item = optionalItem.get();
+		
+		orderItem.setOrder(order);
+		orderItem.setItem(item);
+		return orderItemRepository.save(orderItem);	
+	}
 	
 	/* UPDATE ORDER ITEM */
+	@PutMapping("/orderitem/{id}/{oid}/{iid}")
+	public OrderItem putOrderItem(@PathVariable("id") Long id, 
+							@PathVariable("oid") Long oid, @PathVariable("iid") Long iid) {
+		//get order
+		OrderItem orderItem = getOrderItem(id);
+		
+		//get order
+		Optional<Order> optionalOrder = orderRepository.findById(oid);
+		if (!optionalOrder.isPresent()) {
+			throw new RuntimeException("Order ID is invalid");
+		}
+		Order order = optionalOrder.get();
+		
+		//get item
+		Optional<Item> optionalItem = itemRepository.findById(iid);
+		if (!optionalItem.isPresent()) {
+			throw new RuntimeException("Item ID is invalid");
+		}
+		Item item = optionalItem.get();
+		
+		//update order
+		orderItem.setOrder(order);
+		orderItem.setItem(item);
+		return orderItemRepository.save(orderItem);	
+	}
 	
 	/* DELETE ORDER ITEM BY ID */
 	
