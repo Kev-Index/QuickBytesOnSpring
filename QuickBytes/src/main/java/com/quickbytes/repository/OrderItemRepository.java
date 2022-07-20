3de01edb46fcc816986a2eefe44a06be71197814
Kev-Index <kliu0862@bths.edu>
2022-07-20 09:44:24 -0400

    Imported jpa.repo.Query for OrderItemRepository

--- QuickBytes/src/main/java/com/quickbytes/repository/OrderItemRepository.java
+++ QuickBytes/src/main/java/com/quickbytes/repository/OrderItemRepository.java
@@ -1,6 +1,7 @@
 package com.quickbytes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.quickbytes.model.OrderItem;
 
