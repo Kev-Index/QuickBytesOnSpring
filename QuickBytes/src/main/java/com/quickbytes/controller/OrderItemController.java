18870a97980d32e3d163279a57f153a86fc43381
Kev-Index <kliu0862@bths.edu>
2022-07-20 09:38:43 -0400

    Initial fetch functionality for OrderItem

--- QuickBytes/src/main/java/com/quickbytes/controller/OrderItemController.java
+++ QuickBytes/src/main/java/com/quickbytes/controller/OrderItemController.java
@@ -1,8 +1,15 @@
 package com.quickbytes.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quickbytes.model.Order;
+import com.quickbytes.model.OrderItem;
 import com.quickbytes.repository.OrderItemRepository;
 import com.quickbytes.repository.OrderRepository;
 
@@ -19,12 +26,32 @@ public class OrderItemController {
 	ItemRepository itemRepository;
 
 	/* GET ALL ORDER ITEMS */
+	@GetMapping("/orderitems")
+	public List<OrderItem> getAllOrderItems() {
+		return orderItemRepository.findAll();
+	}
 	
 	/* GET ORDER ITEMS BY ID */
+	@GetMapping("/orderitem/{id}")
+	public OrderItem getOrderItem(@PathVariable("id") Long id) {
+		Optional<OrderItem> orderItem = orderItemRepository.findById(id);
+		if (orderItem.isPresent()) {
+			return orderItem.get();
+		}
+		throw new RuntimeException("ID is invalid");
+	}
 	
 	/* GET ORDER ITEMS BY ORDER ID */
+	@GetMapping("/order/oid/{oid}")
+	public OrderItem getOrderItemsByOrderId(@PathVariable("oid") Long oid) {
+		return orderItemRepository.getOrderItemsByOrderId(oid);
+	}
 	
 	/* GET ORDER ITEMS BY ITEM ID */
+	@GetMapping("/order/iid/{iid}")
+	public OrderItem getOrderItemsByItemId(@PathVariable("iid") Long iid) {
+		return orderItemRepository.getOrderItemsByItemId(iid);
+	}
 	
 	/* POST ORDER ITEM */
 	
--- QuickBytes/src/main/java/com/quickbytes/repository/OrderItemRepository.java
+++ QuickBytes/src/main/java/com/quickbytes/repository/OrderItemRepository.java
@@ -6,4 +6,10 @@ import com.quickbytes.model.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 
+	@Query("select oi from OrderItem oi where oi.order.orderId=?1")
+	OrderItem getOrderItemsByOrderId(Long oid);
+
+	@Query("select oi from OrderItem oi where oi.item.itemId=?1")
+	OrderItem getOrderItemsByItemId(Long iid);
+
 }
