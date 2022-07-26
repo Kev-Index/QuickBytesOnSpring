e783deefd6e249067c9a5f07b9bc651f7c89de00
jayharris14 <jay.harris@centre.edu>
2022-07-26 14:55:59 -0400

    added combo and vendor repositories

--- QuickBytes/src/main/java/com/quickbytes/repository/ComboRepository.java
+++ QuickBytes/src/main/java/com/quickbytes/repository/ComboRepository.java
@@ -1,9 +1,20 @@
 package com.quickbytes.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.quickbytes.model.Combo;
 
 public interface ComboRepository extends JpaRepository<Combo,Long>{
+	@Query("select c from Combo c where c.vendorId=?1")
+	List<Combo> getCombosByVendorId(Long vid);
+	
+	@Query("select c from Combo c where c.name=?1")
+	Optional<Combo> findByname(String name);
 
+
+  
 }
--- QuickBytes/src/main/java/com/quickbytes/repository/VendorRepository.java
+++ QuickBytes/src/main/java/com/quickbytes/repository/VendorRepository.java
@@ -1,9 +1,14 @@
 package com.quickbytes.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.quickbytes.model.Vendor;
 
 public interface VendorRepository extends JpaRepository<Vendor,Long> {
-
+	@Query("select v from Vendor v where v.name=?1")
+	Optional<Vendor> findByname(String name);
+  
 }
