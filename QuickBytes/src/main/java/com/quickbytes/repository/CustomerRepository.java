28ef914ab99e991e6834d1173e71a9357ec84d4a
sshjerry <defineantoine@gmail.com>
2022-08-02 12:37:15 -0700

    Fixing Issue with latest merge of Jerry to main #13

--- QuickBytes/src/main/java/com/quickbytes/repository/CustomerRepository.java
+++ QuickBytes/src/main/java/com/quickbytes/repository/CustomerRepository.java
@@ -1,19 +1,19 @@
-package com.quickbytes.repository;
+package com.quickbytes.backend.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.quickbytes.model.Customer;
+import com.quickbytes.backend.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	@Query("select c from Customer c where c.firstName=?1")
 	List<Customer> getListCustomerWithFirstName(String firstName);
 	@Query("select c from Customer c where c.lastName=?1")
 	List<Customer> getListCustomerWithLastName(String lastName);
-	@Query("select c from Customer c where c.username=?1")
-	Customer getCustomerByUsername(String username);
+	//@Query("select c from Customer c where c.username=?1")
+	//Customer getCustomerByUsername(String username);
 	@Query("select c from Customer c where c.employeeId=?1")
 	Customer getCustomerByEmployeeId(int eid);
 	@Query("select c from Customer c where c.balance>=?1")
