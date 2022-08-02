4852392f98fb22f29d5d2bbdfb7a1e51d763e657
sshjerry <defineantoine@gmail.com>
2022-08-02 12:43:29 -0700

    Fixing Issue with latest merge of Jerry to main #13

--- QuickBytes/src/main/java/com/quickbytes/backend/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quickbytes.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quickbytes.backend.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-
-}
